240a923da820fbeaf2aa5b28392be5ce98a35cbe
steven.laughton@gmail.com <steven.laughton@gmail.com>
2022-04-01 13:51:52 +0100

    fixed lint errors

--- src/Recipes.Web/recipes-app/src/serviceWorkerRegistration.ts
+++ src/Recipes.Web/recipes-app/src/serviceWorkerRegistration.ts
@@ -27,6 +27,7 @@ function registerValidSW(swUrl: string, config?: Config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      // eslint-disable-next-line no-param-reassign
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
@@ -38,6 +39,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
+              // eslint-disable-next-line no-console
               console.log(
                 'New content is available and will be used when all '
                                 + 'tabs for this page are closed. See https://cra.link/PWA.',
@@ -51,6 +53,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
+              // eslint-disable-next-line no-console
               console.log('Content is cached for offline use.');
 
               // Execute callback
@@ -63,6 +66,7 @@ function registerValidSW(swUrl: string, config?: Config) {
       };
     })
     .catch((error) => {
+      // eslint-disable-next-line no-console
       console.error('Error during service worker registration:', error);
     });
 }
@@ -91,6 +95,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
       }
     })
     .catch(() => {
+      // eslint-disable-next-line no-console
       console.log('No internet connection found. App is running in offline mode.');
     });
 }
@@ -116,6 +121,7 @@ export function register(config?: Config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
+          // eslint-disable-next-line no-console
           console.log(
             'This web app is being served cache-first by a service '
                         + 'worker. To learn more, visit https://cra.link/PWA',
@@ -136,6 +142,7 @@ export function unregister() {
         registration.unregister();
       })
       .catch((error) => {
+        // eslint-disable-next-line no-console
         console.error(error.message);
       });
   }
