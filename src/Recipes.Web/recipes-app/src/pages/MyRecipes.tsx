cc65b2fd7c19f5ae32dc2f726f2b292d9f5c46f7
steven.laughton@gmail.com <steven.laughton@gmail.com>
2022-04-24 17:13:01 +0100

    fix: removed console log

--- src/Recipes.Web/recipes-app/src/pages/MyRecipes.tsx
+++ src/Recipes.Web/recipes-app/src/pages/MyRecipes.tsx
@@ -1,7 +1,7 @@
 import {
   IonButtons, IonCol, IonGrid, IonLoading, IonRow,
 } from '@ionic/react';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useFetch } from 'use-http';
 import RecipeCard from '../components/RecipeCard';
 import { RecipeListItem } from '../models/recipe-list-item';
@@ -12,10 +12,6 @@ import FareSegment from '../components/FareSegment';
 function MyRecipes() {
   const [fare, setFare] = useState<Fare>('food');
   const { data: recipeListItems = [], loading } = useFetch(`recipes/getList?fare=${fare}`, {}, [fare]);
-
-  useEffect(() => {
-    console.log(fare);
-  }, [fare]);
 
   return (
     <AppPage
@@ -32,18 +28,6 @@ function MyRecipes() {
       <IonGrid>
         <IonRow>
           <IonLoading isOpen={loading} message="Loading Recipes" />
-          { recipeListItems.map((item: RecipeListItem) => (
-            <IonCol key={item.recipeId} size="6" size-sm="4" size-md="3" size-lg="2" size-xl="1">
-              {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-              <RecipeCard {...item} />
-            </IonCol>
-          ))}
-          { recipeListItems.map((item: RecipeListItem) => (
-            <IonCol key={item.recipeId} size="6" size-sm="4" size-md="3" size-lg="2" size-xl="1">
-              {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-              <RecipeCard {...item} />
-            </IonCol>
-          ))}
           { recipeListItems.map((item: RecipeListItem) => (
             <IonCol key={item.recipeId} size="6" size-sm="4" size-md="3" size-lg="2" size-xl="1">
               {/* eslint-disable-next-line react/jsx-props-no-spreading */}
