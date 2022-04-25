e372d103d8f861299f735f27e64cd3f666b8ef74
steven.laughton@gmail.com <steven.laughton@gmail.com>
2022-04-25 21:32:54 +0100

    feat: added icon

--- src/Recipes.Web/recipes-app/src/components/PortionSelect.tsx
+++ src/Recipes.Web/recipes-app/src/components/PortionSelect.tsx
@@ -1,5 +1,9 @@
-import { IonSelect, IonSelectOption, SelectChangeEventDetail } from '@ionic/react';
+import {
+  IonIcon,
+  IonItem, IonSelect, IonSelectOption, SelectChangeEventDetail,
+} from '@ionic/react';
 import React, { useEffect, useState } from 'react';
+import { peopleOutline } from 'ionicons/icons';
 
 function PortionSelect(
   { recipePortions, setMultiplier }: {
@@ -19,23 +23,26 @@ function PortionSelect(
   };
 
   return (
-    <IonSelect
-      value={portions}
-      onIonChange={(e: CustomEvent<SelectChangeEventDetail<number>>) => {
-        updatePortions(e.detail.value);
-      }}
-    >
-      <IonSelectOption value={1}>1</IonSelectOption>
-      <IonSelectOption value={2}>2</IonSelectOption>
-      <IonSelectOption value={3}>3</IonSelectOption>
-      <IonSelectOption value={4}>4</IonSelectOption>
-      <IonSelectOption value={5}>5</IonSelectOption>
-      <IonSelectOption value={6}>6</IonSelectOption>
-      <IonSelectOption value={7}>7</IonSelectOption>
-      <IonSelectOption value={8}>8</IonSelectOption>
-      <IonSelectOption value={9}>9</IonSelectOption>
-      <IonSelectOption value={10}>10</IonSelectOption>
-    </IonSelect>
+    <IonItem color="light">
+      <IonIcon slot="start" icon={peopleOutline} />
+      <IonSelect
+        value={portions}
+        onIonChange={(e: CustomEvent<SelectChangeEventDetail<number>>) => {
+          updatePortions(e.detail.value);
+        }}
+      >
+        <IonSelectOption value={1}>1</IonSelectOption>
+        <IonSelectOption value={2}>2</IonSelectOption>
+        <IonSelectOption value={3}>3</IonSelectOption>
+        <IonSelectOption value={4}>4</IonSelectOption>
+        <IonSelectOption value={5}>5</IonSelectOption>
+        <IonSelectOption value={6}>6</IonSelectOption>
+        <IonSelectOption value={7}>7</IonSelectOption>
+        <IonSelectOption value={8}>8</IonSelectOption>
+        <IonSelectOption value={9}>9</IonSelectOption>
+        <IonSelectOption value={10}>10</IonSelectOption>
+      </IonSelect>
+    </IonItem>
   );
 }
 
