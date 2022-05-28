652c6b116e8dca96b316c9d8670a56b9bd18c47d
steven.laughton@gmail.com <steven.laughton@gmail.com>
2022-05-28 16:16:07 +0100

    done some logging

--- src/Recipes.Api/IoC/LoggingWebApplicationBuilderExtensions.cs
+++ src/Recipes.Api/IoC/LoggingWebApplicationBuilderExtensions.cs
@@ -7,7 +7,7 @@ public static class LoggingWebApplicationBuilderExtensions
 {
     public static WebApplicationBuilder ConfigureLogging(this WebApplicationBuilder builder)
     {
-        builder.Services.AddLogging(log => log.AddApplicationInsights());
+        // builder.Services.AddLogging(log => log.AddApplicationInsights());
         var options = builder.Configuration.GetSection("ApplicationInsights").Get<ApplicationInsightsServiceOptions>();
         builder.Services.AddApplicationInsightsTelemetry(options);
         builder.Services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, o) =>
--- src/Recipes.Core/Services/GetRecipeList.cs
+++ src/Recipes.Core/Services/GetRecipeList.cs
@@ -2,6 +2,9 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Recipes.Azure.Models;
 using Recipes.Infrastructure;
 using Recipes.Infrastructure.Dtos;
 using Recipes.Infrastructure.Enums;
@@ -14,17 +17,28 @@ public class GetRecipeList : IRequestHandler<GetRecipeListRequest, IList<RecipeL
 {
     private readonly DatabaseContext _context;
     private readonly IConfigurationProvider _configurationProvider;
+    private readonly ILogger<GetRecipeList> _logger;
+    private readonly AzureBlobConfiguration _azureBlobConfiguration;
 
-    public GetRecipeList(DatabaseContext context, IMapper mapper)
+    public GetRecipeList(DatabaseContext context, IMapper mapper, ILogger<GetRecipeList> logger,
+        IOptions<AzureBlobConfiguration> azureBlob)
     {
         _context = context ?? throw new ArgumentNullException(nameof(context));
         _configurationProvider = mapper.ConfigurationProvider ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _azureBlobConfiguration = azureBlob.Value ?? throw new ArgumentNullException(nameof(azureBlob));
     }
 
     public async Task<IList<RecipeListItemDto>> Handle(GetRecipeListRequest request,
         CancellationToken cancellationToken)
     {
-        var recipeListItemDtos = await _context.Recipes
+        _logger.LogInformation(_azureBlobConfiguration.ContainerName);
+        _logger.LogTrace(_azureBlobConfiguration.ContainerName);
+        _logger.LogDebug(_azureBlobConfiguration.ContainerName);
+        _logger.LogWarning(_azureBlobConfiguration.ContainerName);
+        _logger.LogError(_azureBlobConfiguration.ContainerName);
+        _logger.LogCritical(_azureBlobConfiguration.ContainerName);
+        var recipeListItems = await _context.Recipes
             .AsSplitQuery()
             .Where(recipe => recipe.Fare == request.Fare)
             .OrderBy(recipe => recipe.Name)
@@ -36,6 +50,6 @@ public class GetRecipeList : IRequestHandler<GetRecipeListRequest, IList<RecipeL
             })
             .ToListAsync(cancellationToken);
 
-        return recipeListItemDtos;
+        return recipeListItems;
     }
 }
