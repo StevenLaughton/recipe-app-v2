ee4e08c7956d2737821fb1f62b5e3f91ca13dbf0
steven.laughton@gmail.com <steven.laughton@gmail.com>
2022-06-05 11:43:57 +0100

    feat: lighthouse performance, code splitting

--- src/Recipes.Ui2/components/pages/Tabs.tsx
+++ src/Recipes.Ui2/components/pages/Tabs.tsx
@@ -1,22 +1,35 @@
 import {Redirect, Route} from 'react-router-dom';
-import {IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from '@ionic/react';
+import {
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet,
+    IonSpinner,
+    IonTabBar,
+    IonTabButton,
+    IonTabs
+} from '@ionic/react';
 import {add, readerOutline, search} from 'ionicons/icons';
-import Recipes from "./Recipes";
-import Add from './Add';
-import View from "./View";
 import routes from "../../models/constants/routes";
-import Edit from "./Edit";
+import {lazy, Suspense} from "react";
+
+const Recipes = lazy(() => import("./Recipes"));
+const Add = lazy(() => import("./Add"));
+const View = lazy(() => import("./View"));
+const Edit = lazy(() => import("./Edit"));
+
 
 const Tabs = () => {
     return (
         <IonTabs>
             <IonRouterOutlet>
-                <Route path={routes.home} component={Recipes} exact={true}/>
-                <Route path={routes.viewDef} component={View} exact={true}/>
-                {/*<Route path="/tabs/lists" component={Lists} exact={true}/>*/}
-                <Route path={routes.add} component={Add} exact={true}/>
-                <Route path={routes.editDef} component={Edit} exact={true}/>
-                <Route path="/tabs" render={() => <Redirect to={routes.home}/>} exact={true}/>
+                <Suspense fallback={<IonSpinner/>}>
+                    <Route path={routes.home} component={Recipes} exact={true}/>
+                    <Route path={routes.viewDef} component={View} exact={true}/>
+                    {/*<Route path="/tabs/lists" component={Lists} exact={true}/>*/}
+                    <Route path={routes.add} component={Add} exact={true}/>
+                    <Route path={routes.editDef} component={Edit} exact={true}/>
+                    <Route path="/tabs" render={() => <Redirect to={routes.home}/>} exact={true}/>
+                </Suspense>
             </IonRouterOutlet>
             <IonTabBar slot="bottom">
                 <IonTabButton tab="recipes" href={routes.home}>
