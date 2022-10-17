260e1649394ed197fb14ec83df8eddbf8a216fce
StevenLaughton <steven.laughton@audacia.co.uk>
2022-10-17 20:52:04 +0100

    test build

--- src/recipes.ui/src/components/form/RecipeForm.tsx
+++ src/recipes.ui/src/components/form/RecipeForm.tsx
@@ -17,8 +17,7 @@ interface Props {
 }
 
 function RecipeForm({ form, onSubmit }:Props) {
-  return (
-  // eslint-disable-next-line react/jsx-props-no-spreading
+    return (
     <FormProvider {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <IonCard className="my-auto mx-auto w-full shadow-lg">
@@ -30,29 +29,29 @@ function RecipeForm({ form, onSubmit }:Props) {
             <AppInput name="portions" placeholder="Portions" inputmode="numeric" autoCapitalize="off" />
             <ErrorMessage control="portions" state={form.formState} />
           </div>
-          <div className="px-2 py-2 rounded-xl text-white flex justify-between">
-            <IonLabel className="inline-block align-middle">Vegetarian: </IonLabel>
-            <Controller
-              render={({ field: { onChange, value } }) => (
-                <IonToggle
-                  onIonChange={(e: CustomEvent<ToggleChangeEventDetail>) => onChange(e?.detail.checked)}
-                  checked={value}
-                  color="success"
-                />
-              )}
-              name="isVegetarian"
-              control={form.control}
-            />
-          </div>
-          <div className="px-1 py-1 rounded-xl text-white">
-            <Controller
-              render={({ field: { onChange, value } }) => (
-                <FareSegment state={value} setState={onChange} />
-              )}
-              name="fare"
-              control={form.control}
-            />
-          </div>
+          {/*<div className="px-2 py-2 rounded-xl text-white flex justify-between">*/}
+          {/*  <IonLabel className="inline-block align-middle">Vegetarian: </IonLabel>*/}
+          {/*  <Controller*/}
+          {/*    render={({ field: { onChange, value } }) => (*/}
+          {/*      <IonToggle*/}
+          {/*        onIonChange={(e: CustomEvent<ToggleChangeEventDetail>) => onChange(e?.detail.checked)}*/}
+          {/*        checked={value}*/}
+          {/*        color="success"*/}
+          {/*      />*/}
+          {/*    )}*/}
+          {/*    name="isVegetarian"*/}
+          {/*    control={form.control}*/}
+          {/*  />*/}
+          {/*</div>*/}
+          {/*<div className="px-1 py-1 rounded-xl text-white">*/}
+          {/*  <Controller*/}
+          {/*    render={({ field: { onChange, value } }) => (*/}
+          {/*      <FareSegment state={value} setState={onChange} />*/}
+          {/*    )}*/}
+          {/*    name="fare"*/}
+          {/*    control={form.control}*/}
+          {/*  />*/}
+          {/*</div>*/}
         </IonCard>
         <IonListHeader>
           <IonLabel>Ingredients</IonLabel>
