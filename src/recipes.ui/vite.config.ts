75b4a5aa0f9c7911fb9e9e15b3007be56b0606c7
StevenLaughton <steven.laughton@audacia.co.uk>
2022-10-17 21:51:34 +0100

    correct sw

--- src/recipes.ui/vite.config.ts
+++ src/recipes.ui/vite.config.ts
@@ -1,11 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
+
+const pwaManifest: Partial<VitePWAOptions> = {
+    base: '/',
+    registerType: 'prompt',
+    manifest: {
+        background_color: "#008f00",
+        description: "my recipe app",
+        dir: "ltr",
+        display: "standalone",
+        name: "recipe-app",
+        orientation: "any",
+        scope: "/",
+        short_name: "recipes",
+        start_url: "/",
+        theme_color: "#008f00",
+        icons: [
+            {
+                "src": "assets/icon/bowl.svg",
+                "sizes": "64x64 32x32 24x24 16x16",
+                "type": "image/x-icon"
+            },
+            {
+                "src": "assets/icon/bowl.svg",
+                "type": "image/png",
+                "sizes": "512x512",
+                "purpose": "maskable any"
+            }
+        ]
+    }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        VitePWA({ registerType: 'autoUpdate' })
+        VitePWA(pwaManifest)
     ]
 })
